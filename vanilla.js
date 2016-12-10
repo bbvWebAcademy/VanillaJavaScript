f4fe5685c47806dae75ccdf28d984fcbd24c7799
Sourceress <britta.labud@bbv.ch>
2016-12-10 15:11:12 +0100

    Persist tasks in LocalStorage

--- vanilla.js
+++ vanilla.js
@@ -14,12 +14,33 @@ button.addEventListener('click', function(){
  
     inputField.value = "";
     inputField.focus();
+
+    var tasks = localStorage.getItem('tasks');
+    if (!tasks){
+      tasks = '';
+    }
+    else {
+      tasks += ',';
+    }
+    localStorage.setItem('tasks', tasks + input);
   }
 
 });
 
 removebutton.addEventListener('click', function(){
- while (container.childElementCount)
-  container.removeChild(container.lastChild);
+  while (container.childElementCount)
+      container.removeChild(container.lastChild);
+
+  localStorage.removeItem('tasks');
 });
 
+
+var items = localStorage.getItem('tasks');
+if(items){
+  for(var input of items.split(',')){
+      var task = document.createElement("DIV");
+      task.classList.add('taskBox');
+      task.appendChild(document.createTextNode(input));
+      container.appendChild(task);  
+  }
+}
