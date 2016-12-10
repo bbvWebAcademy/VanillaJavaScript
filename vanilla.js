3701c784db34058d9157510a3c6af7f8baf4ee94
Sourceress <britta.labud@bbv.ch>
2016-12-10 14:15:45 +0100

    EventHandlers added

---
+++ vanilla.js
@@ -0,0 +1,26 @@
+var container = document.getElementById('taskContainer');
+var button = document.getElementById('add');
+var inputField = document.querySelector('input');
+var removebutton = document.getElementById('remove');
+
+button.addEventListener('click', function(){
+  var input = inputField.value;
+
+  if(input){
+    var task = document.createElement("DIV");
+    task.classList.add('taskBox');
+    task.appendChild(document.createTextNode(input));
+    container.appendChild(task);
+ 
+    inputField.value = "";
+    inputField.focus();
+  }
+
+});
+
+removebutton.addEventListener('click', function(){
+  localStorage.removeItem('tasks');
+ while (container.childElementCount)
+  container.removeChild(container.firstChild);
+});
+
